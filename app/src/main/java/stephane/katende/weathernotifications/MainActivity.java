f52141a58cef6ac0f1d2deb5eeaee886e6fd66b4
Stephanekkk <swar.kung@gmail.com>
2021-03-23 15:52:49 -0400

    Main branch files This is the latest entire app

---
+++ app/src/main/java/stephane/katende/weathernotifications/MainActivity.java
@@ -0,0 +1,14 @@
+package stephane.katende.weathernotifications;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
